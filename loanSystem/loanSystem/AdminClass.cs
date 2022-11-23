56aca9470f997308e66b5cf234d40e6a958d5363
MassirQ <aqauomi@gmail.com>
2022-11-23 13:30:19 +0100

    add AdminClass

---
+++ loanSystem/loanSystem/AdminClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace loanSystem
+{
+	public class AdminClass
+	{
+		public string name;
+		public string phoneNumber;
+		public string eMail;
+		public enum role
+        {
+            Editor,
+            Viewer
+        }
+
+        public AdminClass(string name, string phoneNumber, string eMail, enum role)
+		{
+			adminName = name;
+			number = phoneNumber;
+			eMail = e-mail;
+			role = AdminRoles; 
+
+
+		}
+
+	//funtion
+
+
+
+	}
+}
+
