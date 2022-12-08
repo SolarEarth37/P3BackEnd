d796150528c3afb310bce59edde48a80e9bd72a7
MassirQ <aqauomi@gmail.com>
2022-12-09 00:42:35 +0100

    add test framework

---
+++ loanSystem/loanSystem.UnitTests/DeviceLenderTests.cs
@@ -0,0 +1,21 @@
+namespace loanSystem.UnitTests;
+
+public class Tests
+{
+    [TestFixture]
+    public class DeviceLenderTests
+    {
+        
+        [Test]
+        public void DeviceLender_Scenario_Exeptaions()
+        {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+            
+            Assert.Pass();
+        }
+    }
+    }
